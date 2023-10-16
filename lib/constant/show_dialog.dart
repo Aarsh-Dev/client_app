b4b4c5fcc7aa11be73cc2db2ed62756e7363232c
Aarsh-Dev <124020010+Aarsh-Dev@users.noreply.github.com>
2023-10-16 18:19:17 +0530

    commit

--- lib/constant/show_dialog.dart
+++ lib/constant/show_dialog.dart
@@ -23,7 +23,7 @@ class ShowDialogs{
               child: Image.asset(AssetPath.imgTestOffer,fit: BoxFit.cover,width: Get.width,),
             ),
            Padding(
-             padding: EdgeInsets.symmetric(horizontal: 8.0),
+             padding: const EdgeInsets.symmetric(horizontal: 8.0),
              child: Column(
                crossAxisAlignment: CrossAxisAlignment.start,
                children: [
